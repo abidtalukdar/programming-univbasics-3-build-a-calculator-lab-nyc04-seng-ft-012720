b997abe69e933557bd4f2d69d7969787f4a4a641
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-13 00:42:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,22 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  return difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
-
+  remainder 
 end
 
 def square_root(num)
